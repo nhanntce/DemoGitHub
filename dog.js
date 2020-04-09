c0419a0baf93b8598b515ed401f3043a79d748f5
Nguyen Thanh Nhan <nguyennhan3639@gmail.com>
2020-04-09 20:19:32 +0700

    add run method on dog class

--- dog.js
+++ dog.js
@@ -2,8 +2,14 @@ function dog(arguments) {
 	
 }
 
+
 dog.prototype.jump = function () {
 	console.log("Jump..");
 }
+
+dog.prototype.run = function() {
+	console.log('Running...');
+};
+
 
 module.exports = dog;
